26da35b8cbabab814e3704049df4dc568908a448
José Victor <josevictornascimento2016@gmail.com>
2025-02-02 16:16:56 -0300

    fear: create use case and file attachment controller

---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+
+@Controller("/attachments")
+export class UploadAttachmentsController {
+  @Post()
+  @UseInterceptors(FileInterceptor("file"))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 2, // 2mb
+          }),
+          new FileTypeValidator({ fileType: ".(png|jpg|jpeg|pdf)" }),
+        ],
+      })
+    )
+    file: Express.Multer.File
+  ) {}
+}
